c42e50d641c2cf6b635639fdad7a271ac0f9f5ef
hweij <henk000@gmail.com>
2025-01-27 12:41:05 +0100

    feat: add help text to remote window

--- main/remote_window.js
+++ main/remote_window.js
@@ -2,9 +2,56 @@
 /// <reference path="../types.d.ts" />
 
 import * as path from "path";
-import { BrowserWindow } from "electron";
+import { BrowserWindow, WebContentsView } from "electron";
+
+const infoHTML = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <style>
+        body {
+            height: 100vh;
+            overflow: hidden;
+            font-family: Arial, Helvetica, sans-serif;
+            font-weight: bold;
+            padding: 0px;
+            margin: 0px;
+            color: #ffffff;
+            background-color: #1A5276;
+            font-size: 18px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding-left: 10px;
+            animation-name: pulse;
+            animation-duration: 1.5s;
+            animation-iteration-count: infinite;
+        }
+        @keyframes pulse {
+            0%,
+            100% {
+                background-color: #1A5276;
+            }
+
+            50% {
+                background-color: #2980B9;
+            }
+        }
+    </style>
+  </head>
+  <body>
+    To add a web-app, select a zip-file and download it.
+  </body>
+</html>`;
 
 export class RemoteWindow extends BrowserWindow {
+    /** @type WebContentsView */
+    vInfo;
+
+    /** @type WebContentsView */
+    vContent;
+
     /** Directory for site download and installation */
     sitesDir = "";
     /** Current remote URL */
@@ -28,7 +75,23 @@ export class RemoteWindow extends BrowserWindow {
         });
         this.sitesDir = sitesDir;
         this.onReady = onReady;
-        this.webContents.session.on('will-download', this.onDownload.bind(this));
+
+        this.vInfo = new WebContentsView();
+        this.contentView.addChildView(this.vInfo);
+        this.vInfo.webContents.loadURL(`data:text/html;charset=UTF-8,${encodeURIComponent(infoHTML)}`);
+
+        this.vContent = new WebContentsView();
+        this.contentView.addChildView(this.vContent);
+        this.vContent.webContents.session.on('will-download', this.onDownload.bind(this));
+
+        this.on("resize", this.updateLayout);
+        this.updateLayout();
+    }
+
+    updateLayout = () => {
+        const bounds = this.contentView.getBounds();
+        this.vInfo.setBounds({ x: 0, y: 0, width: bounds.width, height: 40 });
+        this.vContent.setBounds({ x: 0, y: 40, width: bounds.width, height: bounds.height - 40 });
     }
 
     /**
@@ -37,7 +100,7 @@ export class RemoteWindow extends BrowserWindow {
      */
     setURL(url) {
         if (url !== this.currentURL) {
-            this.webContents.loadURL(url);
+            this.vContent.webContents.loadURL(url);
             this.currentURL = url;
         }
     }
